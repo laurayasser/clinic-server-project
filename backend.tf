c6f99cb3e17d69df2a7ac7830c2fe8658a0f5173
laurayasser <152518649+laurayasser@users.noreply.github.com>
2025-11-28 19:08:20 +0200

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "jenkins-s3-923102106536"
+    bucket       = "csproject923102106536"
     key          = "eks/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
